75e3561bae706f4175f556aaec9a1c64d30ab227
dracul <johandavidgp@gmail.com>
2022-11-10 21:21:24 -0500

    preloader

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ function App() {
 
 
   return (
-    
     <div className="App">
       
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+import logo from'./assets/img/logo.png'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import { wait } from '@testing-library/user-event/dist/utils';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+window.onload = function(){
+  var body = this.document.querySelector('body');
+  var preloader = this.document.getElementById('pre');
+  setTimeout(function(){
+    preloader.style.opacity='50%'
+    preloader.style.opacity='0';
+    body.style.display='flex';
+    body.style.overflow='scroll';
+    console.log(preloader);
+    console.log('cargado');
+  },3000)
+  
+}
 root.render(
   <React.StrictMode>
     <App />
