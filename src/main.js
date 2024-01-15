798027d6f76f50843b60aa58fbe110dfbbcd065a
johan david <johandavidgp@gmail.com>
2024-01-14 21:54:37 -0500

    franken builted

--- src/main.js
+++ src/main.js
@@ -21,24 +21,43 @@ function skillsEfect(){
     const skills_distance = window.innerHeight - skills.getBoundingClientRect().top;
     if(skills_distance >=300){
         let progress = document.getElementsByClassName('progress');
-        progress[0].classList.add('python');
-        progress[1].classList.add('javascript');
-        progress[2].classList.add('htmlcss');
-        progress[3].classList.add('godot');
-        progress[4].classList.add('inks');
-        progress[5].classList.add('kottlin');
-        progress[6].classList.add('tableau');
-        progress[7].classList.add('git');
-        progress[8].classList.add('terminal');
-        progress[9].classList.add('bash');
-        progress[10].classList.add('sql');
-        progress[11].classList.add('docker');
-        progress[12].classList.add('comunication');
-        progress[13].classList.add('teamwork');
-        progress[14].classList.add('creativity');
-        progress[15].classList.add('strategic');
-        progress[16].classList.add('leadership');
-        progress[17].classList.add('activelearning');
+        
+        progress[0].classList.add('Python');
+        
+        progress[1].classList.add('Javascript');
+        
+        progress[2].classList.add('HtmlCss');
+        
+        progress[3].classList.add('Godot');
+        
+        progress[4].classList.add('Inkscape');
+        
+        progress[5].classList.add('Kotlin');
+        
+        progress[6].classList.add('Tableu');
+        
+        progress[7].classList.add('Git');
+        
+        progress[8].classList.add('Terminal');
+        
+        progress[9].classList.add('Bash');
+        
+        progress[10].classList.add('SQL');
+        
+        progress[11].classList.add('Docker');
+        
+        progress[12].classList.add('Comunication');
+        
+        progress[13].classList.add('Teamwork');
+        
+        progress[14].classList.add('Creativity');
+        
+        progress[15].classList.add('StrategicThinking');
+        
+        progress[16].classList.add('Leadership');
+        
+        progress[17].classList.add('ActiveLearning');
+        
     }else{
         console.log('skills desactivados')
 }
@@ -77,9 +96,11 @@ window.addEventListener('load', () =>{
         element.addEventListener('click', () => {
             const path = element.getAttribute('src');
             const description = element.parentNode.parentNode.dataset.description;
+            const post_link = element.parentNode.parentNode.dataset.link;
             gridOverlay.classList.add('active-overlay');
             document.querySelector('.grid-overlay img').src = path;
             document.querySelector('.grid-overlay .description').innerHTML = description;
+            document.querySelector('.grid-overlay .postlink').innerHTML = post_link;
 
         })
     });
